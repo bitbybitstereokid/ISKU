d75905d565916498c97f195ae3717ad2f5d36509
Anton Vasylchenko <vasylchenko.anton1997@gmail.com>
2023-03-14 12:05:25 +0200

    14/03 add js contact

--- assets/component-contact-text.js
+++ assets/component-contact-text.js
@@ -1,17 +1,21 @@
-let btnTitel = document.querySelector(".option-form_title");
-let dropDownList = document.querySelector(".option-form_dropdown-menu ul");
-let itemsList = document.querySelectorAll(".option-form_dropdown-menu ul li");
-let inputChose = document.querySelector(".option_form input");
+window.addEventListener('DOMContentLoaded', () => {
 
-btnTitel.addEventListener("click", function () {
-    this.classList.toggle("_active");
-    dropDownList.classList.toggle("_active");
-})
+    let btnTitel = document.querySelector(".option-form_title");
+    let dropDownList = document.querySelector(".option-form_dropdown-menu ul");
+    let itemsList = document.querySelectorAll(".option-form_dropdown-menu ul li");
+    let inputChose = document.querySelector(".option_form input");
 
-itemsList.forEach((item) => {
-    item.addEventListener("click", function () {
-        dropDownList.classList.remove("_active");
-        btnTitel.innerHTML = item.textContent;
-        inputChose.setAttribute("value", item.textContent);
+    btnTitel.addEventListener("click", function () {
+        this.classList.toggle("_active");
+        dropDownList.classList.toggle("_active");
     })
-})
+
+    itemsList.forEach((item) => {
+        item.addEventListener("click", function () {
+            dropDownList.classList.remove("_active");
+            btnTitel.innerHTML = item.textContent;
+            inputChose.setAttribute("value", item.textContent);
+        })
+    })
+
+});
