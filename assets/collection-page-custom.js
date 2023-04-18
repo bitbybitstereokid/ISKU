c15fee4f20b91e2f998f9c5279b6e03b5ab0026c
Andrew Karapcninskyi <49448634+bitbybitstereokid@users.noreply.github.com>
2023-04-18 21:26:09 +0300

    main files liquid

---
+++ assets/collection-page-custom.js
@@ -0,0 +1,41 @@
+let btnHeight = document.querySelector('.collection-hero--producer-button button');
+let colDesc = document.querySelector('.collection-hero-producer--desc');
+
+if (btnHeight) {
+    btnHeight.addEventListener('click', (e) => {
+        if (colDesc.classList.contains("active")) {
+            colDesc.classList.remove("active");
+            colDesc.style.height = colDesc.scrollHeight + "px";
+        } else {
+            colDesc.classList.add("active");
+            colDesc.style.height = '189px';
+        }
+    });
+}
+function clickSort() {
+    document.querySelector('.custom-select-list').classList.toggle('is-active');
+}
+
+function clickOption(op) {
+    let customOption = document.querySelectorAll('.custom-select-list ul li button');
+    let colTitle = document.querySelector('.custom-select-list .custom-select__title');
+    let formSelect = document.querySelector('#FacetSortForm');
+    let optionData = op.dataset.value;
+    customOption.forEach((optionLi) => {
+        optionLi.classList.remove('selected');
+    });
+
+    const text = optionData;
+    let $select = document.querySelector('#SortBy');
+    let $options = Array.from($select.options);
+    let optionToSelect = $options.find(item => item.value === text);
+    optionToSelect.selected = true;
+    formSelect.dispatchEvent(new Event('input'));
+    op.classList.add('selected');
+    colTitle.innerHTML = op.innerHTML;
+}
+
+
+// document.addEventListener('click', function(e) {
+//     if (!e.target.matches('.custom-select-list')) document.querySelector('.custom-select-list').classList.remove('is-active');
+// });
