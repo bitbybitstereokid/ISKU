663cb99168602e78c854e0e687eacdb07a48a153
Max Chambers <106538302+Uvidest@users.noreply.github.com>
2023-05-17 14:45:56 +0300

    added featured for change media on the multi product

--- assets/global.js
+++ assets/global.js
@@ -873,11 +873,13 @@ class VariantSelects extends HTMLElement {
     if (!this.currentVariant.featured_media) return;
 
     const mediaGalleries = document.querySelectorAll(`[id^="MediaGallery-${this.dataset.section}"]`);
-    mediaGalleries.forEach(mediaGallery => mediaGallery.setActiveMedia(`${this.dataset.section}-${this.currentVariant.featured_media.id}`, true));
+    let currentFeaturedMediaId = this.currentVariant.featured_media.id
+    if ( !document.querySelector(`[data-media-id="${this.dataset.section}-${this.currentVariant.featured_media.id}"]`)) return
+    mediaGalleries.forEach(mediaGallery => mediaGallery.setActiveMedia(`${this.dataset.section}-${currentFeaturedMediaId}`, true));
 
     const modalContent = document.querySelector(`#ProductModal-${this.dataset.section} .product-media-modal__content`);
     if (!modalContent) return;
-    const newMediaModal = modalContent.querySelector( `[data-media-id="${this.currentVariant.featured_media.id}"]`);
+    const newMediaModal = modalContent.querySelector( `[data-media-id="${currentFeaturedMediaId}"]`);
     modalContent.prepend(newMediaModal);
   }
 
